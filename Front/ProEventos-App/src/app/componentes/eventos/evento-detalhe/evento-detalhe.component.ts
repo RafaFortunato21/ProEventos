7fbba8e2456a2321fa554b0c8a90203006d05983
Rafael <fael.fortunato@gmail.com>
2022-08-09 19:20:22 -0300

    Secao 11: Parcial

--- Front/ProEventos-App/src/app/componentes/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/componentes/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+ import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 
 import { Evento } from '@app/models/Evento';
 import { EventoService } from '@app/services/evento.service';
+import { Lote } from '@app/models/Lote';
 
 
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
+import { FormattedError } from '@angular/compiler';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -21,6 +23,11 @@ export class EventoDetalheComponent implements OnInit {
   evento = {} as Evento;
   estadoSalvar = 'post'
   form: FormGroup = new FormGroup({}) ;
+
+  get lotes(): FormArray{
+    return this.form.get('lotes') as FormArray;
+  }
+
 
   get f(): any {
     return this.form.controls;
@@ -104,31 +111,40 @@ export class EventoDetalheComponent implements OnInit {
 
   public validation(): void{
     this.form = this.fb.group({
-      tema: ['',
-        [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
-
+      tema:       ['',[Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
       local:      ['', Validators.required],
-
       dataEvento: ['', Validators.required],
       qtdPessoas: ['',[Validators.required, Validators.max(120000)]],
-
       imageURL:   ['', Validators.required],
       telefone:   ['', Validators.required],
-      email:      ['',[Validators.required, Validators.email]
-      //lotes: this.fb.array([])
+      email:      ['',[Validators.required, Validators.email]],
+      lotes: this.fb.array([])
 
-      ]
+
     });
   }
 
+  adicionarLote(): void {
+    this.lotes.push(this.criarLote({id: 0} as Lote));
+  }
 
+  criarLote(lote: Lote): FormGroup {
+    return this.fb.group({
+      id:[lote.id],
+      nome:[lote.nome, Validators.required],
+      preco:[lote.preco, Validators.required],
+      quantidade:[lote.quantidade, Validators.required],
+      dataInicio:[lote.dataInicio, Validators.required],
+      dataFim:[lote.dataFim, Validators.required]
+    })
+  }
 
   public resetForm(): void {
     this.form.reset();
   }
 
-  public cssValidator(form: FormControl): any {
-    return {'is-invalid': form.errors && form?.touched }
+  public cssValidator(form: FormControl | AbstractControl): any {
+    return {'is-invalid': form?.errors && form?.touched }
   }
 
 }
