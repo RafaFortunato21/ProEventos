6847551399ba0804e60b4f7162ad69508c320563
Rafael <fael.fortunato@gmail.com>
2022-08-14 19:38:20 -0300

    Secao 12: Parcial

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -95,7 +96,7 @@ namespace ProEventos.API.Controllers
                 if (file.Length > 0)
                 {
                     DeleteImage(evento.ImageURL);
-                    //evento.ImageURL = SaveImage(file);
+                    evento.ImageURL = await SaveImage(file);
                 }
                 var EventoRetorno = await _eventoService.UpdateEventos(eventoId, evento);
 
@@ -172,14 +173,40 @@ namespace ProEventos.API.Controllers
         }
 
         [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile) {
+
+            
+
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName)
+                                             .Take(10)
+                                             .ToArray()
+                                             ).Replace(' ', '-');
+
+            imageName = $"{imageName}{DateTime.UtcNow.ToString("yyyymmssfff")} {Path.GetExtension(imageFile.FileName)}";
+
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath,@"Resources/images",imageName);
+
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+
+
+            return imageName;
+
+
+
+        }
+
+
+        [NonAction]
         public void DeleteImage(string imageName) {
             
             var imagePath =  Path.Combine(_hostEnvironment.ContentRootPath, @"Resources/images", imageName);
 
             if (System.IO.File.Exists(imagePath))
-            {
                 System.IO.File.Delete(imagePath);
-            }
+
         }
     }
 }
