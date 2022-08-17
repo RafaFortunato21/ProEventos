ad3fac6ffa724f8ba81b83e96c9f0c022dde09da
Rafael Fortunato <fael.fortunato@gmail.com>
2022-08-17 18:04:13 -0300

    Secao 13: Parcial

---
+++ Back/src/ProEventos.Domain/Identity/Role.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using ProEventos.Domain.Enum;
+
+namespace ProEventos.Domain.Identity
+{
+    public class Role : IdentityRole<int>
+    {
+        public IEnumerable<UserRole> UserRoles { get; set; }
+        
+    
+    }
+}
