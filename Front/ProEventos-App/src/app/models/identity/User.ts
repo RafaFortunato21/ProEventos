846fdc4f26e10e35fa923ca27ae15205d885dacf
Rafael Fortunato <fael.fortunato@gmail.com>
2022-08-24 12:46:57 -0300

    Secao 14 - Parcial

---
+++ Front/ProEventos-App/src/app/models/identity/User.ts
@@ -0,0 +1,12 @@
+
+
+export class User {
+  userName: string;
+  email: string;
+  password: string;
+  primeiroNome: string;
+  ultimoNomte: string;
+  token: string;
+
+
+}
--- Front/ProEventos-App/src/app/services/evento.service.ts
+++ Front/ProEventos-App/src/app/services/evento.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Evento } from '../models/Evento';
@@ -8,40 +8,41 @@ import { environment } from '@environments/environment';
 @Injectable( )
 export class EventoService {
   baseURL = environment.apiURL + 'api/eventos/'
+  tokenHeader = new HttpHeaders({ 'Authorization': 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIzIiwidW5pcXVlX25hbWUiOiJkb3VnbGFzMiIsIm5iZiI6MTY2MTE5MjcxMywiZXhwIjoxNjYxMjc5MTEzLCJpYXQiOjE2NjExOTI3MTN9.fLMkHq1oV9dkMs5tdBOljGWMEYQRiv6eJUCYXW_ux2SoY48QFaG7hAQfWuPpumCS4fq2ZgogFXNRIhm6YwR7Fg' });
 
   constructor(private http:HttpClient) { }
 
   public getEvento(): Observable<Evento[]> {
-    return this.http.get<Evento[]>(this.baseURL).pipe(take(1));
+    return this.http.get<Evento[]>(this.baseURL, {headers: this.tokenHeader} ).pipe(take(1));
   }
 
   public getEventosByTema(tema: string): Observable<Evento[]> {
     return this.http
-      .get<Evento[]>(`${this.baseURL}${tema}/tema`)
+      .get<Evento[]>(`${this.baseURL, {headers: this.tokenHeader}}${tema}/tema`)
       .pipe(take(1));
   }
 
   public getEventoById(id: number): Observable<Evento> {
     return this.http
-      .get<Evento>(`${this.baseURL}${id}`)
+      .get<Evento>(`${this.baseURL, {headers: this.tokenHeader}}${id}`)
       .pipe(take(1));
   }
 
   public post(evento: Evento): Observable<Evento> {
     return this.http
-      .post<Evento>(`${this.baseURL}`,evento)
+      .post<Evento>(`${this.baseURL, {headers: this.tokenHeader}}`,evento)
       .pipe(take(1));
   }
 
   public put(evento: Evento): Observable<Evento> {
     return this.http
-    .put<Evento>(`${this.baseURL}${evento.id}`, evento)
+    .put<Evento>(`${this.baseURL, {headers: this.tokenHeader}}${evento.id}`, evento)
     .pipe(take(1));
   }
 
   public deleteEvento(id:number): Observable<any> {
     return this.http
-      .delete(`${this.baseURL}${id}`)
+      .delete(`${this.baseURL, {headers: this.tokenHeader}}${id}`)
       .pipe(take(1));
   }
 
@@ -53,7 +54,7 @@ export class EventoService {
     formData.append('file', fileToUpload);
 
     return this.http
-      .post<Evento>(`${this.baseURL}upload-image/${eventoId}`, formData)
+      .post<Evento>(`${this.baseURL, {headers: this.tokenHeader}}upload-image/${eventoId}`, formData)
       .pipe(take(1));
   }
 
